eb3273b4fcd115b245fcd90030759ed92f974029
Thiago Menezes <thiagocwbpr@gmail.com>
2023-02-07 12:16:21 -0300

    Modificado a forma das funções na classe Menu para melhor visibilidade. Foi criado um método 'Moldura' que dentro desse método, contém duas funções que criam as linhas verticais e horizontais da moldura do Menu. Foi retirado essa responsabilidade do método DrawScreen no qual somente irá chamar o método 'Moldura' daqui pra frente.

--- Menu.cs
+++ Menu.cs
@@ -14,34 +14,35 @@ namespace HTMLeditor {
             Console.ForegroundColor = ConsoleColor.Black;
 
             DrawScreen();
-        }
+        } //  Responsavel pelo gerenciamento do Menu.
         public static void DrawScreen() {
-
+            Moldura();
+        } // Responsavel pelo layout da tela.
+        public static void Moldura() { 
             Cabecalho();
             Colunas();
             Cabecalho();
-            
-        }
 
-        public static void Cabecalho() {
-            Console.Write("+");
-            for (int i = 0; i <= 30; i++)
-                Console.Write("-");
-
-            Console.Write("+");
-            Console.Write("\n");
-        }
-
-        public static void Colunas() {
-            for (int lines = 0; lines <= 10; lines++) {
-                Console.Write("|");
+            static void Cabecalho() {
+                Console.Write("+");
                 for (int i = 0; i <= 30; i++)
-                    Console.Write(" ");
+                    Console.Write("-");
 
-                Console.Write("|");
+                Console.Write("+");
                 Console.Write("\n");
-            }
-        }
+            } //<-- Cria as linhas horizontais da moldura do Menu.
+            static void Colunas() {
+                for (int lines = 0; lines <= 10; lines++) {
+                    Console.Write("|");
+                    for (int i = 0; i <= 30; i++)
+                        Console.Write(" ");
+
+                    Console.Write("|");
+                    Console.Write("\n");
+                }
+
+            }   //<-- Cria as colunas verticais da moldura do Menu.
+        }   // Responsavel pela criação da moldura do Menu.
 
     }
 }
