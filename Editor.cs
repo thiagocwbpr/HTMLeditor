5fc76d8e2a952743132fcfc14b63803bf9b55e75
Thiago Menezes <thiagocwbpr@gmail.com>
2023-02-08 12:07:59 -0300

    Criado uma classe chamada 'viewer' no qual será responsável por visualizar o texto com a string HTML. nesse caso, foi criado uma função chamada 'Replace' que capta a string HTML e modica o texto conforme sua natureza. A string 'strong' em HTML, torna o texto em negrito, portanto nosso texto irá ter a cor azul, para mostrar que realmente o strong funciona no código.

--- Editor.cs
+++ Editor.cs
@@ -9,8 +9,8 @@ namespace HTMLeditor {
 
         public static void Show() {
             Console.Clear();
-            Console.BackgroundColor= ConsoleColor.White;
-            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
             Console.Clear();
             Console.WriteLine("MODO EDITOR");
             Console.WriteLine("-----------");
@@ -22,11 +22,12 @@ namespace HTMLeditor {
 
             do {
                 file.Append(Console.ReadLine());
-                file.Append(" "+ Environment.NewLine);
+                file.Append(Environment.NewLine);
             } while (Console.ReadKey().Key != ConsoleKey.Escape);
 
             Console.WriteLine("-----------");
-            Console.WriteLine("  Deseja salvar o arquivo?");
+            Console.WriteLine(" Deseja salvar o arquivo?");
+            Viewer.Show(file.ToString());
         }
     }
 }
